668a10d03fe7d8cf2c9b1f11750ea7a60eb217e3
Joel Martin <github@martintribe.org>
2021-10-06 10:09:59 -0500

    Simplify veth-link.sh using more iproute2 features.
    - The `ip link add` command supports a netns argument for both ends of
      the link which allows us to skip the separate insert steps.
    - The `ip netns attach` command creates a netns to process ID mapping
      that removes the need for the manual directory creation and symlinking
      step.
    
    Also:
    - Re-order argument processing to reduce operation and setup mangling.
    - Add a couple more informative messages to clarify which step we are
      in even when not using verbose logging.

--- veth-link.sh
+++ veth-link.sh
@@ -21,10 +21,27 @@ usage () {
     exit 2
 }
 
-# Parse arguments
 VERBOSE=${VERBOSE:-}
-IP0= IP1= MAC0= MAC1= PID0= PID1= CID0= CID1=
-POSITIONAL=
+IP0= IP1= MAC0= MAC1= PID0= PID1=
+
+info() { echo "veth-link [${PID0}/${IF0} <-> ${PID1}/${IF1}] ${*}"; }
+warn() { >&2 echo "veth-link [${PID0}/${IF0} <-> ${PID1}/${IF1}] ${*}"; }
+die () { warn "ERROR: ${*}"; exit 1; }
+
+# Set MAC, IP, and up state for interface within netns
+setup_if() {
+  local IF=$1 NS=$2 MAC=$3 IP=$4
+
+  ip -netns ${NS} --force -b - <<EOF
+      ${IP:+addr add ${IP} dev ${IF}}
+      ${MAC:+link set dev ${IF} address ${MAC}}
+      link set dev ${IF} up
+      netns del ${NS}
+EOF
+}
+
+# Parse arguments
+positional=
 while [ "${*}" ]; do
   param=$1; OPTARG=$2
   case ${param} in
@@ -34,81 +51,35 @@ while [ "${*}" ]; do
   --mac0) MAC0="${OPTARG}"; shift ;;
   --mac1) MAC1="${OPTARG}"; shift ;;
   -h|--help) usage ;;
-  *) POSITIONAL="${POSITIONAL} $1" ;;
+  *) positional="${positional} $1" ;;
   esac
   shift
 done
-set -- ${POSITIONAL}
+set -- ${positional}
+IF0=$1 IF1=$2 PID0=$3 PID1=$4
 
-IF0=$1
-IF1=$2
-PID0=$3
-PID1=$4
-
-info() {
-    echo "veth-link [${PID0}/${IF0} <-> ${PID1}/${IF1}] ${*}"
-}
-
-warn() {
-    >&2 echo "veth-link [${PID0}/${IF0} <-> ${PID1}/${IF1}] ${*}"
-}
-
-die () {
-    warn "ERROR: ${*}"
-    exit 1
-}
-
-info "Creating veth pair link (${IP0}|${MAC0} <-> ${IP1}|${MAC1})"
+[ "${VERBOSE}" ] && set -x || true
 
 # Check arguments
 [ "${IF0}" -a "${IF1}" -a "${PID0}" -a "${PID1}" ] || usage
 
-[ "${VERBOSE}" ] && set -x || true
-
-TMP_IF0=T0-${PID0}
-TMP_IF1=T1-${PID1}
-
 # Sanity checks
 [ ! -d /proc/$PID0 ] && die "PID0 $PID0 is no longer running!"
 [ ! -d /proc/$PID1 ] && die "PID1 $PID1 is no longer running!"
-ip link show "${TMP_IF0}" > /dev/null 2>&1 && die "Interface ${TMP_IF0} exists"
-ip link show "${TMP_IF1}" > /dev/null 2>&1 && die "Interface ${TMP_IF1} exists"
-
-
-insert_if() {
-  local ORIG_IF=$1 IF=$2 PID=$3 MAC=$4 IP=$5 OTHER_IP=$6
-
-  # Create a netns links to the container network namespace
-  NSID=ns${PID}
-  mkdir -p /var/run/netns
-  rm -f /var/run/netns/${NSID}
-  ln -sf /proc/${PID}/ns/net /var/run/netns/${NSID}
-
-  # Inject the veth interface into the container
-  ip link set ${ORIG_IF} netns ${NSID}
-
-  # Setup the inner interface
-  ip -netns ${NSID} --force -b - <<EOF
-      link set ${ORIG_IF} name ${IF}
-      ${IP:+addr add ${IP} dev ${IF}}
-      ${MAC:+link set dev ${IF} address ${MAC}}
-      link set dev ${IF} up
-      netns del ${NSID}
-EOF
-      #${OTHER_IP:+route replace ${OTHER_IP} dev ${IF}}
-      #${OTHER_IP:+route replace default via ${OTHER_IP%/*} dev ${IF}}
-
-}
 
 ### Do the work
 
-#info "Assigning ${IP} to ${CID:0:12}, VRIP: ${VRIP}, PID: ${PID}"
+info "Creating veth pair link (${IP0}|${MAC0} <-> ${IP1}|${MAC1})"
 
-# Create the veth pair
-ip link add ${TMP_IF0} type veth peer name ${TMP_IF1}
+info "Creating ip netns to pid mappings"
+ip netns attach ns${PID0} ${PID0}
+ip netns attach ns${PID1} ${PID1}
 
-# Insert the ends into the containers
-insert_if "${TMP_IF0}" "${IF0}" "${PID0}" "${MAC0}" "${IP0}" "${IP1}"
-insert_if "${TMP_IF1}" "${IF1}" "${PID1}" "${MAC1}" "${IP1}" "${IP0}"
+info "Creating veth pair with ends in each namespace"
+ip link add ${IF0} netns ns${PID0} type veth peer ${IF1} netns ns${PID1}
+
+info "Setting MAC, IP, and up state"
+setup_if ${IF0} ns${PID0} "${MAC0}" "${IP0}"
+setup_if ${IF1} ns${PID1} "${MAC1}" "${IP1}"
 
 info "Created veth pair link (${IP0}|${MAC0} <-> ${IP1}|${MAC1})"
